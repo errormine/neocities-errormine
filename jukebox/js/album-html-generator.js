dc89ba2d00e5d75d4157788b8630957dfd3cca4e
Andrei Neacsu <andnea823@gmail.com>
2023-11-02 14:00:25 -0500

    Remove the jukebox page... bye bye :(

--- jukebox/js/album-html-generator.js
+++
@@ -1,9 +0,0 @@
-function generateHTML(artist, albumName, songName) {
-    return `
-    <li>
-        <img id='${songName.toLowerCase().replaceAll(' ', '-')}' class='album-cover' src='/jukebox/img/covers/${artist.toLowerCase().replaceAll(' ', '-')-albumName.toLowerCase().replaceAll(' ', '-')}.jpg' alt=''>
-        <h5>${albumName}</h5>
-        <p>${artist}</p>
-        <a href=''>View on Discogs</a>
-    </li>`
-}
